4960ee3400d934ddb6723ff964136a12209b7900
Aleksander Borowski <73070465+happyRip@users.noreply.github.com>
2021-04-19 18:55:04 +0200

    implemented CLI using Cobra
    usage:
    
    * `typer` is used to generate random words by default.
    
    *  `--file` / `-f` followed by filename is used to read input from file instead.
    
    * `--length` / `-l` followed by an integer is used to set number of max words, by default is set to max int value.
    
    * '--capital' / '-c' is used to indicate that the user wants to include capital letters from text, by default capital letters are changed to lowercase.
    
    * `--punctuation` / `-p` is used to indicate that the user wants to include punctuation from text, by default punctuation is removed.
    
    All groups of whitespace characters are replaced with a single space character by design.
    
    The app lacks description to be included with Cobra.

--- go.mod
+++ go.mod
@@ -1,13 +1,15 @@
-module github.com/maaslalani/typer
+module typer
 
 go 1.16
 
 require (
-	github.com/charmbracelet/bubbles v0.7.6
-	github.com/charmbracelet/bubbletea v0.12.2
+	github.com/charmbracelet/bubbles v0.7.7 // indirect
+	github.com/charmbracelet/bubbletea v0.13.2 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/mitchellh/go-wordwrap v1.0.1 // indirect
-	github.com/muesli/termenv v0.8.0 // indirect
-	github.com/onsi/ginkgo v1.15.1 // indirect
-	github.com/onsi/gomega v1.11.0 // indirect
-	github.com/tjarratt/babble v0.0.0-20191209142150-eecdf8c2339d
+	github.com/mosaicnetworks/babble v0.8.4 // indirect
+	github.com/muesli/termenv v0.8.1 // indirect
+	github.com/spf13/cobra v1.1.3 // indirect
+	github.com/spf13/viper v1.7.1 // indirect
+	github.com/tjarratt/babble v0.0.0-20191209142150-eecdf8c2339d // indirect
 )
---
+++ pkg/utility/utility.go
@@ -0,0 +1,65 @@
+package utility
+
+import (
+	"os"
+	"regexp"
+	"strings"
+
+	"github.com/tjarratt/babble"
+)
+
+const (
+	MaxUint = ^uint(0)
+	MaxInt  = int(MaxUint >> 1)
+)
+
+// ReadFile returns the file contents as a string
+func ReadFile(path string) (string, error) {
+	contents, err := os.ReadFile(path)
+	return string(contents), err
+}
+
+// RandomWords generates a strings with the specified number of random words using Babble
+func RandomWords(n int) string {
+	babbler := babble.NewBabbler()
+	babbler.Separator = " "
+	babbler.Count = n
+	return babbler.Babble()
+}
+
+// AdjustLength shortens a string if it's word count is greater than n
+func AdjustLength(s string, n int) string {
+	words := strings.Fields(s)
+	if len(words) > n {
+		words = words[:n]
+	}
+	s = strings.Join(words, " ")
+
+	return s
+}
+
+// AdjustWhitespace replaces every group of whitespace characters with a single space charracter
+func AdjustWhitespace(s string) (string, error) {
+	reg, err := regexp.Compile(`\s+`)
+	if err != nil {
+		return "", err
+	}
+
+	s = reg.ReplaceAllString(s, " ")
+
+	if s[len(s)-1] == ' ' {
+		s = s[:len(s)-1]
+	}
+	return s, nil
+}
+
+// RemoveNonAlpha removes all non-alphanumeric characters exept spaces
+func RemoveNonAlpha(s string) (string, error) {
+	reg, err := regexp.Compile(`[^a-zA-Z0-9\s]+`)
+	if err != nil {
+		return "", err
+	}
+
+	s = reg.ReplaceAllString(s, "")
+	return s, nil
+}
