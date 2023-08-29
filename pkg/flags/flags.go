179769ae6cfa613c3754f17d114c98add3f94065
arif <arif-celik56@hotmail.com>
2023-08-29 23:23:56 +0300

    feat: added quote option

--- pkg/flags/flags.go
+++ pkg/flags/flags.go
@@ -2,6 +2,7 @@ package flags
 
 import (
 	"fmt"
+	"math/rand"
 	"strings"
 
 	util "github.com/maaslalani/typer/pkg/utility"
@@ -17,10 +18,20 @@ type Flags struct {
 	MinWordLength int
 	Capital       bool
 	Punctuation   bool
+	Quote         bool
 }
 
-// formatText applies formatting based on flags
-func (f *Flags) FormatText(s string) (string, error) {
+// It is not very correct to have Quote in this file, but if it is in typer, it causes an import cycle
+// TODO make a new file for Quote or something
+type Quote struct {
+	Text   string `json:"text"`
+	Source string `json:"source"`
+	Length int    `json:"length"`
+	Id     int    `json:"id"`
+}
+
+// formatWords applies formatting based on flags
+func (f *Flags) FormatWords(s string) (string, error) {
 	var err error
 	s, err = util.AdjustWhitespace(s)
 	if err != nil {
@@ -57,4 +68,34 @@ func (f *Flags) FormatText(s string) (string, error) {
 	}
 
 	return s, nil
+}
+
+// formatQuote applies formatting based on flags for quotes.
+// this method ignores the p and c flags because they are quotes
+// returns random quote from struct
+func (f *Flags) FormatQuote(q struct {
+	Quotes []Quote `json:"quotes"`
+}) (string, error) {
+	var quotes []string
+
+	if f.Length <= 0 {
+		f.Length = DefaultLength
+	} else if f.Length > MaxLength {
+		f.Length = MaxLength
+	}
+
+	// for quotes, f.Length is the max length of the quote because there is no
+	// information about words count in json file, so we will filter by letters count
+	// and use f.Length as max length
+	for _, q := range q.Quotes {
+		if q.Length >= f.Length {
+			s, err := util.AdjustWhitespace(q.Text)
+			if err != nil {
+				return "", err
+			}
+			quotes = append(quotes, s)
+		}
+	}
+
+	return quotes[rand.Intn(len(quotes))], nil
 }
